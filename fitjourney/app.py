5e96e2c2e2b6da901f36ba6c489f3cbd85ecdadb
Davaladarshini <davaladarshini01@gmail.com>
2025-11-18 17:39:15 +0530

    updated routes in html files

--- fitjourney/app.py
+++
@@ -1,9 +0,0 @@
-from . import create_app
-
-# Create the app instance using the factory
-app = create_app()
-
-if __name__ == '__main__':
-    # You can configure host and port here if needed
-    # Example: app.run(debug=True, host='0.0.0.0', port=5000)
-    app.run(debug=True)
